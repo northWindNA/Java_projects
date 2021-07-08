35746f6ac69edb5960a0e183e43f996ac0a58ea1
mhumfrey <apn0597@gmail.com>
2021-07-08 20:15:50 +0300

    exceptions

---
+++ exceptions/src/exceptions.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class exceptions {
+    public static void main(String[] args) throws ScannerException {
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            int x = Integer.parseInt(scan.nextLine());
+            if (x != 0) {
+                throw new ScannerException("non-zero input");
+            }
+        }
+    }
+}
